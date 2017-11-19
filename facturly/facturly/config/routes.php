93a2180fc83d076edee53522673ffaf0b7f3f8ff
Manik Mahajan <manikmahajan9090@gmail.com>
2017-11-20 03:49:22 +0530

    changes

--- facturly/facturly/config/routes.php
+++ facturly/facturly/config/routes.php
@@ -50,6 +50,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |		my-controller/my-method	-> my_controller/my_method
 */
 $route['default_controller'] = 'login';
+
 $route['logout'] = 'Login/logout';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
